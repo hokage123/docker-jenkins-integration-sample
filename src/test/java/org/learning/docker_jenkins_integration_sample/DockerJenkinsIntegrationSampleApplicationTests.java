c3d8a4d88ea96dc041303f6e5bd340cea12cdf80
hokage123 <39802344+hokage123@users.noreply.github.com>
2024-07-18 23:43:08 +0530

    first commit

---
+++ src/test/java/org/learning/docker_jenkins_integration_sample/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.docker_jenkins_integration_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
